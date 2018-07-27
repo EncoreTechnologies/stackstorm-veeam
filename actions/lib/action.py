8689a92a8e600033ea2ebad0ab852eaeebe0ca05
Nick Maludy <nmaludy@gmail.com>
2018-07-27 19:45:38 -0400

    Added base action

---
+++ actions/lib/action.py
@@ -0,0 +1,32 @@
+from st2common.runners.base_action import Action
+
+import requests
+import xmltodict
+
+
+class BaseAction(Action):
+
+    def login(self, **kwargs):
+        self.base_url = "{}://{}:{}/api".format(kwargs['scheme'],
+                                                kwargs['server'],
+                                                kwargs['port'])
+        self.session = requests.Session()
+        self.session.auth = (kwargs['username'], kwargs['password'])
+        self.session.verify = kwargs['ssl_verify']
+
+        self.post("/sessionMngr/?v=latest")
+        return self.session
+
+    def request(self, methood endpoint, payload=None):
+        response = self.session.request(method,
+                                        "{}{}".format(self.base_url, endpoint),
+                                        json=payload)
+        response.raise_for_status()
+        # parse the respose from XML into a "dict"
+        return xmltodict.parse(response.content)
+
+    def post(self, endpoint, payload=None):
+        return self.request("POST", endpoint, payload=payload)
+
+    def get(self, endpoint, payload=None):
+        return self.request("GET", endpoint, payload=payload)
